a552dfdf098625743ed727a82ec9c1d5e454b73b
JIABI <bijia@me.com>
2023-09-15 11:11:02 +0100

    Update FIne_tuning_bert.py

--- FIne_tuning_bert.py
+++ FIne_tuning_bert.py
@@ -5,12 +5,21 @@ import torch.nn as nn
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 import transformers
-from transformers import AutoModel, BertTokenizerFast
+from transformers import AutoModel, BertTokenizerFast, RobertaTokenizer, RobertaModel
 import time
-
+import argparse
+from transformers import AdamW
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from train import BERT_Arch
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', type=str, default='roberta-base', help='set model type')
+parser.add_argument('--batch_size', type=int, default=32, help='set batch size')
+parser.add_argument('--epochs', type=int, default=10, help='set number of epochs')
+parser.add_argument('--retrain', type=str, default='True', help='set retrain')
+args = parser.parse_args()
 # specify GPU
 device = torch.device("cuda")
-
+results_file_path = "result.txt"
 # Load Dataset
 df = pd.read_csv("spamdata_v2.csv")
 df.head()
@@ -29,142 +38,109 @@ val_text, test_text, val_labels, test_labels = train_test_split(temp_text, temp_
                                                                 random_state=2018,
                                                                 test_size=0.5,
                                                                 stratify=temp_labels)
+def bert():
+    if args.model == 'roberta-base':
+        bert = RobertaModel.from_pretrained(args.model)
+    else:
+        bert = AutoModel.from_pretrained(args.model)
+    # Freeze BERT Parameters
+    for param in bert.parameters():
+        param.requires_grad = False
+    return bert
+def load_model():
+    # Import BERT Model and BERT Tokenizer
+    # import BERT-base pretrained model
+    # Load the BERT tokenizer
+    if args.model == 'roberta-base':
+        tokenizer = RobertaTokenizer.from_pretrained(args.model)
+    else:
+        tokenizer = BertTokenizerFast.from_pretrained(args.model)
+    # sample data
+    text = ["this is a bert model tutorial", "we will fine-tune a bert model"]
+    # encode text
+    sent_id = tokenizer.batch_encode_plus(text, padding=True, return_token_type_ids=False)
 
-# Import BERT Model and BERT Tokenizer
-# import BERT-base pretrained model
-bert = AutoModel.from_pretrained('bert-base-uncased')
-# Load the BERT tokenizer
-tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
-# sample data
-text = ["this is a bert model tutorial", "we will fine-tune a bert model"]
-# encode text
-sent_id = tokenizer.batch_encode_plus(text, padding=True, return_token_type_ids=False)
-# output
-print(sent_id)
+    # output
+    print(sent_id)
 
-# Tokenization
-# get length of all the messages in the train set
-seq_len = [len(i.split()) for i in train_text]
-pd.Series(seq_len).hist(bins = 30)
-max_seq_len = 25
-# tokenize and encode sequences in the training set
-tokens_train = tokenizer.batch_encode_plus(
-    train_text.tolist(),
-    max_length = max_seq_len,
-    pad_to_max_length=True,
-    truncation=True,
-    return_token_type_ids=False
-)
+    # Tokenization
+    # get length of all the messages in the train set
+    seq_len = [len(i.split()) for i in train_text]
+    pd.Series(seq_len).hist(bins=30)
+    max_seq_len = 25
+    # tokenize and encode sequences in the training set
+    tokens_train = tokenizer.batch_encode_plus(
+        train_text.tolist(),
+        max_length=max_seq_len,
+        pad_to_max_length=True,
+        truncation=True,
+        return_token_type_ids=False
+    )
 
-# tokenize and encode sequences in the validation set
-tokens_val = tokenizer.batch_encode_plus(
-    val_text.tolist(),
-    max_length = max_seq_len,
-    pad_to_max_length=True,
-    truncation=True,
-    return_token_type_ids=False
-)
+    # tokenize and encode sequences in the validation set
+    tokens_val = tokenizer.batch_encode_plus(
+        val_text.tolist(),
+        max_length=max_seq_len,
+        pad_to_max_length=True,
+        truncation=True,
+        return_token_type_ids=False
+    )
 
-# tokenize and encode sequences in the test set
-tokens_test = tokenizer.batch_encode_plus(
-    test_text.tolist(),
-    max_length = max_seq_len,
-    pad_to_max_length=True,
-    truncation=True,
-    return_token_type_ids=False
-)
+    # tokenize and encode sequences in the test set
+    tokens_test = tokenizer.batch_encode_plus(
+        test_text.tolist(),
+        max_length=max_seq_len,
+        pad_to_max_length=True,
+        truncation=True,
+        return_token_type_ids=False
+    )
+    # Convert Integer Sequences to Tensors
+    # for train set
+    train_seq = torch.tensor(tokens_train['input_ids'])
+    train_mask = torch.tensor(tokens_train['attention_mask'])
+    train_y = torch.tensor(train_labels.tolist())
+    # for validation set
+    val_seq = torch.tensor(tokens_val['input_ids'])
+    val_mask = torch.tensor(tokens_val['attention_mask'])
+    val_y = torch.tensor(val_labels.tolist())
+    # for test set
+    test_seq = torch.tensor(tokens_test['input_ids'])
+    test_mask = torch.tensor(tokens_test['attention_mask'])
+    test_y = torch.tensor(test_labels.tolist())
+    return [train_seq, train_mask, train_y, val_seq, val_mask, val_y, test_seq, test_mask, test_y]
 
-# Convert Integer Sequences to Tensors
-# for train set
-train_seq = torch.tensor(tokens_train['input_ids'])
-train_mask = torch.tensor(tokens_train['attention_mask'])
-train_y = torch.tensor(train_labels.tolist())
-# for validation set
-val_seq = torch.tensor(tokens_val['input_ids'])
-val_mask = torch.tensor(tokens_val['attention_mask'])
-val_y = torch.tensor(val_labels.tolist())
-# for test set
-test_seq = torch.tensor(tokens_test['input_ids'])
-test_mask = torch.tensor(tokens_test['attention_mask'])
-test_y = torch.tensor(test_labels.tolist())
 
-# Create DataLoaders
-from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
-
-#define a batch size
-batch_size = 32
+# Create DataLoader
 
 # wrap tensors
-train_data = TensorDataset(train_seq, train_mask, train_y)
+train_test_result = load_model()
+train_data = TensorDataset(train_test_result[0], train_test_result[1], train_test_result[2])
 
 # sampler for sampling the data during training
 train_sampler = RandomSampler(train_data)
 
 # dataLoader for train set
-train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=args.batch_size)
 
 # wrap tensors
-val_data = TensorDataset(val_seq, val_mask, val_y)
+val_data = TensorDataset(train_test_result[3], train_test_result[4], train_test_result[5])
 
 # sampler for sampling the data during training
 val_sampler = SequentialSampler(val_data)
 
 # dataLoader for validation set
-val_dataloader = DataLoader(val_data, sampler = val_sampler, batch_size=batch_size)
+val_dataloader = DataLoader(val_data, sampler = val_sampler, batch_size=args.batch_size)
 
-# Freeze BERT Parameters
-for param in bert.parameters():
-    param.requires_grad = False
 
 # Define Model Architecture
-class BERT_Arch(nn.Module):
 
-    def __init__(self, bert):
-        super(BERT_Arch, self).__init__()
-
-        self.bert = bert
-
-        # dropout layer
-        self.dropout = nn.Dropout(0.1)
-
-        # relu activation function
-        self.relu = nn.ReLU()
-
-        # dense layer 1
-        self.fc1 = nn.Linear(in_features=768, out_features=512)
-
-        # dense layer 2 (Output layer)
-        self.fc2 = nn.Linear(in_features=512, out_features=2)
-
-        # softmax activation function
-        self.softmax = nn.LogSoftmax(dim=1)
-
-    # define the forward pass
-    def forward(self, sent_id, mask):
-        # pass the inputs to the model
-        _, cls_hs = self.bert(sent_id, attention_mask=mask, return_dict=False)
-
-        x = self.fc1(cls_hs)
-
-        x = self.relu(x)
-
-        x = self.dropout(x)
-
-        # output layer
-        x = self.fc2(x)
-
-        # apply softmax activation
-        x = self.softmax(x)
-
-        return x
 # pass the pre-trained BERT to our define architecture
-model = BERT_Arch(bert)
+model = BERT_Arch(bert())
 
 # push the model to GPU
 model = model.to(device)
 
 # optimizer from hugging face transformers
-from transformers import AdamW
 
 # define the optimizer
 optimizer = AdamW(model.parameters(), lr = 1e-3)
@@ -303,7 +279,8 @@ best_valid_loss = float('inf')
 # empty lists to store training and validation loss of each epoch
 train_losses = []
 valid_losses = []
-
+import os
+path = os.path.join("saved_"+args.model+".pt")
 # for each epoch
 for epoch in range(epochs):
 
@@ -318,7 +295,8 @@ for epoch in range(epochs):
     # save the best model
     if valid_loss < best_valid_loss:
         best_valid_loss = valid_loss
-        torch.save(model.state_dict(), 'saved_weights.pt')
+        #torch.save(model.state_dict(), 'saved_weights_cased.pt')
+        torch.save(model.state_dict(), path)
 
     # append training and validation loss
     train_losses.append(train_loss)
@@ -330,16 +308,24 @@ for epoch in range(epochs):
 
 # Load Saved Model
 #load weights of best model
-path = 'saved_weights.pt'
+#bert = AutoModel.from_pretrained(args.model)
+# Load the BERT tokenizer
+#tokenizer = BertTokenizerFast.from_pretrained(args.model)
+
+#path = 'saved_weights_cased.pt'
 model.load_state_dict(torch.load(path))
 
-# Get Predictions for Test Data
+    # Get Predictions for Test Data
 with torch.no_grad():
-  preds = model(test_seq.to(device), test_mask.to(device))
-  preds = preds.detach().cpu().numpy()
-# model's performance
-preds = np.argmax(preds, axis = 1)
-print(classification_report(test_y, preds))
+    preds = model(train_test_result[6].to(device), train_test_result[7].to(device))
+    preds = preds.detach().cpu().numpy()
 
-# confusion matrix
-pd.crosstab(test_y, preds)
+    # model's performance
+preds = np.argmax(preds, axis=1)
+result = classification_report(train_test_result[8], preds)
+    # confusion matrix
+pd.crosstab(train_test_result[8], preds)
+
+with open(results_file_path, 'a') as file:
+    file.write("Model, Test accuracy\n")
+    file.write(f"{args.model}, {result}")
